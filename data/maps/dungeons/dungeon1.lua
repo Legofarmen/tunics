dcc8a066e8c535956027c26ada98901c9da8aaa9
root <root@localhost.localdomain>
2015-03-11 21:30:43 +0100

    Added west, east, boss and entrance doors. Centered north and south doors. Added corners and rail to main room

--- data/maps/dungeons/dungeon1.lua
+++ data/maps/dungeons/dungeon1.lua
@@ -10,6 +10,6 @@ map:include(3200-160, 2400-120-240, 'rooms/room1', 'here', 'be', 'dragons')
 local x0 = 3200-160
 local y0 = 2400-120
 
-map:create_dynamic_tile{ layer = 1, x = x0 + 144, y = y0 - 16, width = 16, height = 32, pattern = "ceiling", enabled_at_start = true }
-map:create_dynamic_tile{ layer = 1, x = x0 + 136, y = y0 - 8, width = 8, height = 16, pattern = "barrier.2", enabled_at_start = true }
-map:create_dynamic_tile{ layer = 1, x = x0 + 160, y = y0 - 8, width = 8, height = 16, pattern = "barrier.2", enabled_at_start = true }
+map:create_dynamic_tile{ layer = 1, x = x0 + 152, y = y0 - 16, width = 16, height = 32, pattern = "ceiling", enabled_at_start = true }
+map:create_dynamic_tile{ layer = 1, x = x0 + 144, y = y0 - 8, width = 8, height = 16, pattern = "barrier.2", enabled_at_start = true }
+map:create_dynamic_tile{ layer = 1, x = x0 + 168, y = y0 - 8, width = 8, height = 16, pattern = "barrier.2", enabled_at_start = true }
