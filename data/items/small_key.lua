af45a1dffe587ad5bcb780e4442f4c913e23b0fb
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-03-12 11:32:49 +0100

    Fix broken boss door

--- data/items/small_key.lua
+++ data/items/small_key.lua
@@ -1,7 +1,5 @@
 local item = ...
 
-local game = item:get_game()
-
 function item:on_created()
     self:set_savegame_variable('small_key')
     self:set_amount_savegame_variable('small_key_amount')
--- data/maps/components/door_north.lua
+++ data/maps/components/door_north.lua
@@ -7,12 +7,11 @@ if properties.open then
         y=16,
         direction=1,
     }
-
     if properties.open == 'big_key' then
         my_door.sprite='entities/door_big_key'
         my_door.savegame_variable='door_big_key1'
         my_door.opening_method='interaction_if_savegame_variable'
-        my_door.opening_condition='big_key1'
+        my_door.opening_condition='big_key'
         door(my_door)
     elseif properties.open == 'small_key' then
         my_door.sprite='entities/door_small_key'
