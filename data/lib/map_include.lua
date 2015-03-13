0231d6fcf765c873438fde6fa796edd551541c98
Jan Sandström <jan@legofarmen.se>
2015-03-13 15:47:24 +0100

    Updated map_include with enemies  #Please enter the commit message for your changes. Lines starting

--- data/lib/map_include.lua
+++ data/lib/map_include.lua
@@ -8,6 +8,12 @@ local mapmeta = sol.main.get_metatable('map')
 function properties() end
 function destination() end
 
+function enemy(properties)
+    properties.x = properties.x + x0
+    properties.y = properties.y + y0
+    map0:create_enemy(properties)
+end
+
 function pickable(properties)
     properties.x = properties.x + x0
     properties.y = properties.y + y0
