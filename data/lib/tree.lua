2248e430fa278e3ffee15c2541a10c28ee832f11
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-03-26 09:59:08 +0100

    Disallow multiple normal chests in single room

--- data/lib/tree.lua
+++ data/lib/tree.lua
@@ -278,10 +278,12 @@ function tree.Metric:is_valid()
     if self.hidden_treasures > 1 then return false end
     if self.bigkey_treasures > 1 then return false end
     if self.obstacle_treasures > 1 then return false end
-    if self.bigkey_treasures > 0 and self:get_obstacles() > 0 then return false end
+    if self.normal_treasures > 1 then return false end
     if self.bigkey_treasures > 0 and self.normal_treasures > 0 then return false end
-    if self.hidden_treasures > 0 and self:get_obstacles() > 0 then return false end
-    if self:get_obstacles() > 0 and self.treasures - self.hidden_treasures > 1 then return false end
+    if self:get_obstacles() > 0 then
+        if self.bigkey_treasures > 0 then return false end
+        if self.hidden_treasures > 0 then return false end
+    end
     return true
 end
 
