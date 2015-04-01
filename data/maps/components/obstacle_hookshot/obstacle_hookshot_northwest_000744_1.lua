78ca1625bf8999bc81e5c69e003866898ae1db22
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-04-01 09:13:57 +0200

    Use obstacle hookshot component helper

--- data/maps/components/obstacle_hookshot/obstacle_hookshot_northwest_000744_1.lua
+++ data/maps/components/obstacle_hookshot/obstacle_hookshot_northwest_000744_1.lua
@@ -1,30 +1,5 @@
 local map, data = ...
 
-function replace(placeholder, data)
-    local x, y = placeholder:get_position()
-    x, y = x + 8, y + 13
-    if data then
-        map:create_chest{
-            sprite="entities/chest",
-            layer=1,
-            x = x,
-            y = y,
-            treasure_name=data.item_name,
-            treasure_savegame_variable=data.name,
-        }
-    else
-        map:create_block{
-            layer = 1,
-            x = x,
-            y = y,
-            direction = -1,
-            sprite = "entities/block",
-            pushable = false,
-            pullable = false,
-            maximum_moves = 0,
-        }
-    end
-end
+local hookshot = require 'maps/components/obstacle_hookshot/hookshot'
 
-replace(placeholder1, data.treasure1)
-replace(placeholder2, data.treasure2)
+hookshot.init(map, data)
