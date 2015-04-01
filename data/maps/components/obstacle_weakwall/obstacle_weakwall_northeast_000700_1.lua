a7b25631c2119aa60f45f8d4ca7e7cb1108883cc
Jan Sandström <jan@legofarmen.se>
2015-04-01 17:49:08 +0200

    Rename/cleanup obstacle_weakwall

--- data/maps/components/obstacle_weakwall/obstacle_weakwall_northeast_000700_1.lua
+++
@@ -1,5 +0,0 @@
-local map, data = ...
-
-local bomb = require 'maps/components/obstacle_weakwall/bomb'
-
-bomb.init(map,data)
---
+++ data/maps/components/obstacle_weakwall/obstacle_weakwall_northsoutheast_000001_1.lua
@@ -0,0 +1,5 @@
+local map, data = ...
+
+local bomb = require 'maps/components/obstacle_weakwall/obstacle_weakwall' 
+
+bomb.init(map,data)
