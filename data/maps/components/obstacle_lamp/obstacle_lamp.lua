7c523814fee9cb0ecfabd12df430340f8f7b8519
Jan Sandström <jan@legofarmen.se>
2015-04-02 16:35:40 +0200

    Fixed hidden chests in obstacle_lamp.lua

--- data/maps/components/obstacle_lamp/obstacle_lamp.lua
+++ data/maps/components/obstacle_lamp/obstacle_lamp.lua
@@ -7,42 +7,13 @@ function lamp.init(map, data, timeout)
 	for dir, door_data in pairs(data.doors) do
         data.room:door({open='closed', name=door_data.name, door_names=door_names}, dir)
 	end
-	
-	local torches = map:get_entities('torch_')
-	local torch_count = map:get_entities_count('torch_')
-	local lit_count = 0 
-	for torch in torches do
-		local torch_u = torch:get_userdata()
-		if timeout then torch:set_timeout(timeout) end
-		function torch_u:on_lit()
-			lit_count = lit_count + 1
-		
-			if lit_count == torch_count then
-				local sound = nil
-				if data.item_name then
-					chest:set_enabled(true)
-					sound = 'chest_appears'
-				end
-				for dir, name in pairs(door_names) do
-					map:open_doors(name)
-					sound = 'secret'
-				end
-				sol.audio.play_sound(sound)
-			end	
-        end
-		function torch_u:on_unlighting()
-			if lit_count == torch_count then
-				return false
-			else
-				lit_count = lit_count - 1
-			end
-		end
-	end
+			
+	local hidden_chest = nil
 	
 	if data.treasure1 then
 		local x, y = map:get_entity('treasure_obstacle_chest'):get_position()
 		x, y = x + 8, y + 13
-		map:create_chest{
+		hidden_chest = map:create_chest{
             sprite="entities/chest",
             layer=1,
             x = x,
@@ -50,6 +21,7 @@ function lamp.init(map, data, timeout)
             treasure_name=data.treasure1.item_name,
             treasure_savegame_variable=data.treasure1.name,
         }
+		hidden_chest:set_enabled(false)
 	else
 		map:set_entities_enabled('treasure_obstacle_', false)
 	end
@@ -67,6 +39,36 @@ function lamp.init(map, data, timeout)
         }
 	else
 		map:set_entities_enabled('treasure_open_', false)
+	end
+	
+	local torches = map:get_entities('torch_')
+	local torch_count = map:get_entities_count('torch_')
+	local lit_count = 0 
+	for torch in torches do
+		local torch_u = torch:get_userdata()
+		if timeout then torch:set_timeout(timeout) end
+		function torch_u:on_lit()
+			lit_count = lit_count + 1
+			if lit_count == torch_count then
+				local sound = nil
+				if data.treasure1 then
+					hidden_chest:set_enabled(true)
+					sound = 'chest_appears'
+				end
+				for dir, name in pairs(door_names) do
+					map:open_doors(name)
+					sound = 'secret'
+				end
+				sol.audio.play_sound(sound)
+			end	
+        end
+		function torch_u:on_unlighting()
+			if lit_count == torch_count then
+				return false
+			else
+				lit_count = lit_count - 1
+			end
+		end
 	end
 end
 
