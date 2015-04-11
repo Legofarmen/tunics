8f0fd7fe460375ddd8f0f538bce5631da59039ac
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-04-11 23:43:45 +0200

    Fix boss door

--- data/maps/components/door/door_bigkey_north_200777_1.lua
+++ data/maps/components/door/door_bigkey_north_200777_1.lua
@@ -3,6 +3,7 @@ local map, data = ...
 local zentropy = require 'lib/zentropy'
 
 zentropy.inject_door(map:get_entity('doorway'), {
+    savegame_variable = data.name,
     direction = 1,
     sprite = "entities/door_big_key",
     opening_method = "interaction_if_savegame_variable",
