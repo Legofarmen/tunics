ba599ff45fe22bff6043dd2361c69ca03baa5881
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-04-12 02:34:38 +0200

    Initialize zentropy in on_started

--- data/main.lua
+++ data/main.lua
@@ -1,9 +1,9 @@
 local zentropy = require 'lib/zentropy'
 local game_menu = require 'menus/game_menu'
 
-zentropy.init()
-
 function sol.main:on_started()
+
+    zentropy.init()
     sol.language.set_language("en")
     sol.menu.start(self, game_menu)
 	sol.audio.preload_sounds()
