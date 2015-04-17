70692180227490981f07e0983729d2b7a9a25e77
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-04-17 23:53:22 +0200

    No obstacles in bigkey door room

--- data/lib/tree.lua
+++ data/lib/tree.lua
@@ -328,6 +328,7 @@ function tree.Metric:is_valid()
 
     if self.bigkey_doors > 0 and self.treasures > 0 then return false end
     if self.bigkey_doors > 0 and self.hidden_doors > 0 then return false end
+    if self.bigkey_doors > 0 and self:get_obstacles() > 0 then return false end
 
     if self.treasures > 2 then return false end
     if self.normal_treasures > 1 then return false end
