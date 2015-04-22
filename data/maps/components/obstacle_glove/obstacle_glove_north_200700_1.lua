b061a5a24fb4946755c7576102c4f23c4964228e
thomas <thomas.kwarnmark@gmail.com>
2015-04-22 13:46:51 +0100

    add several more maps to obstacle_glove

--- data/maps/components/obstacle_glove/obstacle_glove_north_200700_1.lua
+++
@@ -1,5 +0,0 @@
-local map, data = ...
-
-local glove = require 'maps/components/obstacle_glove/obstacle_glove'
-
-glove.init(map, data)
---
+++ data/maps/components/obstacle_glove/obstacle_glove_north_200700_2.lua
@@ -0,0 +1,5 @@
+local map, data = ...
+
+local glove = require 'maps/components/obstacle_glove/obstacle_glove'
+
+glove.init(map, data)
