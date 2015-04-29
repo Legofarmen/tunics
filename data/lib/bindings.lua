51bac62ff9a2437a3237595ed56143da0c8006fe
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-04-29 12:03:08 +0200

    New bindings in game menu

---
+++ data/lib/bindings.lua
@@ -0,0 +1,84 @@
+local bindings = {}
+
+local commands = {
+    escape =    { buttons={1}, keys={'escape'} },
+    map =       { buttons={2}, keys={'tab'} },
+    inventory = { buttons={3}, keys={'w'} },
+    attack =    { buttons={4}, keys={'s'} },
+    item_1 =    { buttons={5}, keys={'a'} },
+    item_2 =    { buttons={6}, keys={'d'} },
+    action =    { buttons={7}, keys={'space', 'return', 'enter'} },
+    up =        { keys={'up'} },
+    down =      { keys={'down'} },
+    left =      { keys={'left'} },
+    right =     { keys={'right'} },
+}
+
+local axis_commands = {
+    [0] = {
+        [-1] = 'left',
+        [1] = 'right',
+    },
+    [1] = {
+        [-1] = 'up',
+        [1] = 'down',
+    },
+}
+
+function bindings.mixin(o)
+    local keys = {}
+    local buttons = {}
+    for command, bindings in pairs(commands) do
+        for _, key in ipairs(bindings.keys) do
+            keys[key] = command
+        end
+        for _, button in ipairs(bindings.buttons or {}) do
+            buttons[button] = command
+        end
+    end
+    local axis_state = {}
+
+    function o:on_key_pressed(key, modifiers)
+        if keys[key] and self.on_command_pressed then
+            return self:on_command_pressed(keys[key])
+        end
+    end
+
+    function o:on_key_released(key, modifiers)
+        if keys[key] and self.on_command_released then
+            return self:on_command_released(keys[key])
+        end
+    end
+
+    function o:on_joypad_button_pressed(button, modifiers)
+        if buttons[button] and self.on_command_pressed then
+            return self:on_command_pressed(buttons[button])
+        end
+    end
+
+    function o:on_joypad_button_released(button, modifiers)
+        if buttons[button] and self.on_command_released then
+            return self:on_command_released(buttons[button])
+        end
+    end
+
+    function o:on_joypad_axis_moved(axis, state)
+        local old_state = axis_state[axis] or 0
+        axis_state[axis] = state
+        if state ~= 0 then
+            if self.on_command_released then
+                self:on_command_released(axis_commands[old_state])
+            end
+            if self.on_command_pressed then
+                return self:on_command_pressed(axis_commands[state])
+            end
+        elseif old_state ~= 0 then
+            if self.on_command_released then
+                return self:on_command_released(axis_commands[old_state])
+            end
+        end
+    end
+
+end
+
+return bindings
