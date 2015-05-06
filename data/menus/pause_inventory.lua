dce9f9d3a1fd03478e72e1e59090a09cabb3dc1c
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-06 20:59:03 +0200

    Use item order in inventory menu

--- data/menus/pause_inventory.lua
+++ data/menus/pause_inventory.lua
@@ -1,13 +1,6 @@
 local submenu = require("menus/pause_submenu")
 local inventory_submenu = submenu:new()
 
-local item_names = {
-  "bombs_counter",
-  "bow",
-  "lamp",
-  "hookshot",
-}
-
 function inventory_submenu:on_started()
 
     submenu.on_started(self)
@@ -17,10 +10,22 @@ function inventory_submenu:on_started()
     self.sprites = {}
     self.counters = {}
     self.captions = {}
+    self.assignable_items = {}
+    self.passive_items = {}
 
-    for k = 1, #item_names do
+    for i, item_name in ipairs(zentropy.game.items) do
+        local item = self.game:get_item(item_name)
+        if item:is_assignable() then
+            table.insert(self.assignable_items, item)
+        else
+            table.insert(self.passive_items, item)
+        end
+    end
+
+
+    for k = 1, #self.assignable_items do
         -- Get the item, its possession state and amount.
-        local item = self.game:get_item(item_names[k])
+        local item = self.assignable_items[k]
         local variant = item:get_variant()
 
         if variant > 0 then
@@ -39,7 +44,7 @@ function inventory_submenu:on_started()
 
             -- Initialize the sprite and the caption string.
             self.sprites[k] = sol.sprite.create("entities/items")
-            self.sprites[k]:set_animation(item_names[k])
+            self.sprites[k]:set_animation(item:get_name())
             self.sprites[k]:set_direction(variant - 1)
         end
     end
@@ -80,13 +85,12 @@ function inventory_submenu:set_cursor_position(row, column)
   self.game:set_value("pause_inventory_last_item_index", index)
 
   -- Update the caption text and the action icon.
-  local item_name = item_names[index + 1]
-  local item = item_name and self.game:get_item(item_name) or nil
+  local item = self.assignable_items[index + 1]
   local variant = item and item:get_variant() or 0
 
   local item_icon_opacity = 128
   if variant > 0 then
-    self:set_caption("inventory.caption.item." .. item_name .. "." .. variant)
+    self:set_caption("inventory.caption.item." .. item:get_name() .. "." .. variant)
     self.game:set_custom_command_effect("action", "info")
     if item:is_assignable() then
       item_icon_opacity = 255
@@ -106,8 +110,8 @@ end
 
 function inventory_submenu:is_item_selected()
 
-  local item_name = item_names[self:get_selected_index() + 1]
-  return self.game:get_item(item_name):get_variant() > 0
+  local item = self.assignable_items[self:get_selected_index() + 1]
+  return item and item:get_variant() > 0
 end
 
 function inventory_submenu:on_command_pressed(command)
@@ -188,8 +192,8 @@ function inventory_submenu:on_draw(dst_surface)
 
     for j = 0, 3 do
       k = k + 1
-      if item_names[k] ~= nil then
-        local item = self.game:get_item(item_names[k])
+      if self.assignable_items[k] ~= nil then
+        local item = self.assignable_items[k]
         if item:get_variant() > 0 then
           -- The player has this item: draw it.
           self.sprites[k]:draw(dst_surface, x, y)
@@ -217,8 +221,8 @@ end
 -- The player is supposed to have this item.
 function inventory_submenu:show_info_message()
 
-  local item_name = item_names[self:get_selected_index() + 1]
-  local variant = self.game:get_item(item_name):get_variant()
+  local item = self.assignable_items[self:get_selected_index() + 1]
+  local variant = item:get_variant()
   local map = self.game:get_map()
 
   -- Position of the dialog (top or bottom).
@@ -230,7 +234,7 @@ function inventory_submenu:show_info_message()
 
   self.game:set_custom_command_effect("action", nil)
   self.game:set_custom_command_effect("attack", nil)
-  self.game:start_dialog("_item_description." .. item_name .. "." .. variant, function()
+  self.game:start_dialog("_item_description." .. item:get_name() .. "." .. variant, function()
     self.game:set_custom_command_effect("action", "info")
     self.game:set_custom_command_effect("attack", "save")
     self.game.dialog_box:set_dialog_position("auto")  -- Back to automatic position.
@@ -245,8 +249,7 @@ end
 function inventory_submenu:assign_item(slot)
 
   local index = self:get_selected_index() + 1
-  local item_name = item_names[index]
-  local item = self.game:get_item(item_name)
+  local item = self.assignable_items[index]
 
   -- If this item is not assignable, do nothing.
   if not item:is_assignable() then
@@ -261,7 +264,7 @@ function inventory_submenu:assign_item(slot)
   -- Memorize this item.
   self.item_assigned = item
   self.item_assigned_sprite = sol.sprite.create("entities/items")
-  self.item_assigned_sprite:set_animation(item_name)
+  self.item_assigned_sprite:set_animation(item:get_name())
   self.item_assigned_sprite:set_direction(item:get_variant() - 1)
   self.item_assigned_destination = slot
 
