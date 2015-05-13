24b245c93ad25e103ddfd080cfa816cba8573f57
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-13 22:17:24 +0200

    Whitespace

--- data/main.lua
+++ data/main.lua
@@ -13,27 +13,27 @@ function sol.main:on_started()
     bindings.mixin(legofarmen_logo)
     bindings.mixin(title_screen)
     bindings.mixin(game_menu)
-	
-    zentropy.init()
-		    
-	sol.language.set_language("en")
-    
-	if zentropy.settings.skip_cinematics then
-		sol.menu.start(self, game_menu)
-	else
-		sol.menu.start(self, solarus_logo)
-		solarus_logo.on_finished = function()
-			sol.menu.start(self, legofarmen_logo)
-		end
 
-		legofarmen_logo.on_finished = function()
-			sol.menu.start(self, title_screen)
-		end
-		
-		title_screen.on_finished = function()
-			sol.menu.start(self, game_menu)
-		end
-	end
+    zentropy.init()
+
+    sol.language.set_language("en")
+    
+    if zentropy.settings.skip_cinematics then
+        sol.menu.start(self, game_menu)
+    else
+        sol.menu.start(self, solarus_logo)
+        solarus_logo.on_finished = function()
+            sol.menu.start(self, legofarmen_logo)
+        end
+
+        legofarmen_logo.on_finished = function()
+            sol.menu.start(self, title_screen)
+        end
+
+        title_screen.on_finished = function()
+            sol.menu.start(self, game_menu)
+        end
+    end
 
 end
 
