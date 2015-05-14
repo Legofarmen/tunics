6865e3eafaa2d6666ca48b0c1953e7dff9e90388
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-15 01:45:25 +0200

    Validate tile positions

--- data/validate.lua
+++ data/validate.lua
@@ -1,7 +1,117 @@
 local zentropy = require 'lib/zentropy'
 
+function intersects(r1, r2)
+    if r1.x + r1.width <= r2.x then return false end
+    if r1.x >= r2.x + r2.width then return false end
+    if r1.y + r1.height <= r2.y then return false end
+    if r1.y >= r2.y + r2.height then return false end
+    return true
+end
+
+function validate_entity_layer(fname, description, properties)
+    if properties.layer == 0 then
+        zentropy.debug(string.format("%s at low layer in component: %s", description, fname))
+    end
+end
+
+function validate_entity_mask(fname, description, properties, sections)
+    for i, section in ipairs(sections) do
+        if intersects(section, properties) then
+            zentropy.debug(string.format("%s outside mask in component: %s", description, fname))
+        end
+    end
+end
+
+function read_component_map(fname, mask, tilesets, patterns)
+    local all_sections = {
+        { x = 176, y = 136, width = 120, height = 80, },
+        { x = 144, y = 136, width =  32, height = 80, },
+        { x =  24, y = 136, width = 120, height = 80, },
+        { x = 176, y = 104, width = 120, height = 32, },
+        { x = 144, y = 104, width =  32, height = 32, },
+        { x =  24, y = 104, width = 120, height = 32, },
+        { x = 176, y =  24, width = 120, height = 80, },
+        { x = 144, y =  24, width =  32, height = 80, },
+        { x =  24, y =  24, width = 120, height = 80, },
+    }
+    local i = 1
+    local sections = {}
+    for i = 1, 9 do
+        if bit32.band(mask, bit32.lshift(1, i-1)) == 0 then
+            table.insert(sections, all_sections[i])
+        end
+    end
+
+    local datf = sol.main.load_file(fname)
+    if not datf then
+        error("error: loading file: " .. fname)
+    end
+    local mt = {}
+    function mt.properties(properties)
+        if properties.x ~= 0 or properties.y ~= 0 then
+            zentropy.debug('component origin not (0;0): ' .. fname)
+        end
+        if properties.width ~= 320 or properties.height ~= 240 then
+            zentropy.debug('component size not (320;200): ' .. fname)
+        end
+        if not tilesets[properties.tileset] then
+            zentropy.debug(string.format('unknown tileset (%s) in component: %s', properties.tileset, fname))
+        end
+    end
+    function mt.custom_entity(properties)
+        local description = string.format("custom_entity (%s)", properties.pattern)
+        validate_entity_layer(fname, description, properties)
+        validate_entity_mask(fname, description, properties, sections)
+    end
+    function mt.dynamic_tile(properties)
+        local description = string.format("dynamic tile (%s)", properties.pattern)
+        validate_entity_layer(fname, description, properties)
+        validate_entity_mask(fname, description, properties, sections)
+    end
+    function mt.jumper(properties)
+        local description = string.format("jumper (%s)", properties.pattern)
+        validate_entity_layer(fname, description, properties)
+        validate_entity_mask(fname, description, properties, sections)
+    end
+    function mt.sensor(properties)
+        local description = string.format("sensor (%s)", properties.pattern)
+        validate_entity_layer(fname, description, properties)
+        validate_entity_mask(fname, description, properties, sections)
+    end
+    function mt.tile(properties)
+        local description = string.format("tile (%s)", properties.pattern)
+        validate_entity_layer(fname, description, properties)
+        validate_entity_mask(fname, description, properties, sections)
+    end
+    function mt.wall(properties)
+        local description = string.format("wall (%s)", properties.pattern)
+        validate_entity_layer(fname, description, properties)
+        validate_entity_mask(fname, description, properties, sections)
+    end
+    function mt.block(properties) end
+    function mt.bomb(properties) end
+    function mt.chest(properties) end
+    function mt.crystal(properties) end
+    function mt.crystal_block(properties) end
+    function mt.destination(properties) end
+    function mt.destructible(properties) end
+    function mt.door(properties) end
+    function mt.enemy(properties) end
+    function mt.explosion(properties) end
+    function mt.fire(properties) end
+    function mt.npc(properties) end
+    function mt.pickable(properties) end
+    function mt.separator(properties) end
+    function mt.shop_treasure(properties) end
+    function mt.stairs(properties) end
+    function mt.stream(properties) end
+    function mt.switch(properties) end
+    function mt.teletransporter(properties) end
+    setfenv(datf, mt)()
+end
+
 function validate_projectdb_components()
-    local patterns = get_patterns(zentropy.tilesets)
+    local tilesets, patterns = get_patterns(zentropy.tilesets)
     --[[
     for k, v in pairs(zentropy.components.floors) do
         zentropy.debug('floor', k, v)
@@ -18,17 +128,18 @@ function validate_projectdb_components()
     for k, v in pairs(zentropy.components.enemies) do
         zentropy.debug('enemy', k, v)
     end
-    for k, v in pairs(zentropy.components.fillers) do
-        for j, u in pairs(v) do
-            zentropy.debug('filler', k, j, u)
-        end
-    end
     ]]
+    local fmt = "maps/%s.dat"
+    for k, v in pairs(zentropy.components.fillers) do
+        read_component_map(string.format(fmt, v.id), v.mask, tilesets, patterns)
+    end
+    --[[
     zentropy.debug_callback(function (write)
         for id, size in pairs(patterns) do
             write(string.format("%2dx%2d %s", size.width, size.height, id))
         end
     end)
+    ]]
 end
 
 function read_tileset_tiles(fname)
@@ -50,6 +161,7 @@ end
 
 function get_patterns(tilesets)
     local patterns = {}
+    local tileset_names = {}
     local tileset_count = 0
     local fmt = "tilesets/%s.dat"
     for k, v in pairs(tilesets) do
@@ -68,6 +180,7 @@ function get_patterns(tilesets)
                     end
                     table.insert(patterns[id].tilesets, tileset)
                 end
+                tileset_names[tileset] = true
             end
         end
     end
@@ -75,10 +188,10 @@ function get_patterns(tilesets)
         if #info.tilesets == tileset_count then
             info.tilesets = nil
         else
-            error("tile pattern not present in all tilesets: " .. id)
+            error(string.format("tile pattern present in %d of %d tilesets: %s", #info.tilesets, tileset_count, id))
         end
     end
-    return patterns
+    return tileset_names, patterns
 end
 
 validate_projectdb_components()
