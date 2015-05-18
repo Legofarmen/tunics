ed1564571fe8e8ee31a8a06ae6f2670f45c7f6f8
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-18 15:12:35 +0200

    Refactor boss entrance

--- data/maps/components/door/door_bigkey.lua
+++ data/maps/components/door/door_bigkey.lua
@@ -4,12 +4,27 @@ local door_bigkey = {}
 
 function door_bigkey.init(map, data, direction)
 
-    zentropy.inject_door(map:get_entity('door_open'), {
-        direction = direction,
-        sprite = "entities/door_big_key",
-        opening_method = "interaction_if_item",
-        opening_condition = "bigkey",
-    })
+    if direction == 1 then
+        zentropy.inject_door(map:get_entity('door_open'), {
+            name = 'door',
+            direction = direction,
+            sprite = "entities/door_big_key",
+            opening_method = "interaction_if_item",
+            opening_condition = "bigkey",
+        })
+    else
+        local door = zentropy.inject_door(map:get_entity('door_open'), {
+            name = 'door',
+            direction = direction,
+            sprite = "entities/door_normal",
+        })
+        map:set_doors_open(door:get_name(), true)
+
+        data.room_events:add_door_sensor_activated_listener(function ()
+            map:close_doors(door:get_name())
+        end)
+
+    end
 
     local sensor = map:get_entity('sensor')
 
