37599f20e7aa8425f830fd297d6d2157ea035b6a
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-18 12:18:58 +0200

    Weighted component selection

--- data/lib/zentropy.lua
+++ data/lib/zentropy.lua
@@ -291,6 +291,17 @@ function zentropy.get_door_mask(dir)
     return door_mask
 end
 
+local function get_component_size(mask)
+    local count = 0
+    while mask > 0 do
+        if bit32.band(mask, 1) ~= 0 then
+            count = count + 1
+        end
+        mask = bit32.rshift(mask, 1)
+    end
+    return count
+end
+
 function zentropy.db.Components:get_obstacle(item, dir, mask, rng)
     if not self.obstacles[item] then
         return
@@ -313,47 +324,50 @@ function zentropy.db.Components:get_obstacle(item, dir, mask, rng)
         doors[d] = true
     end
 
-    local entries = {}
+    local chosen = nil
+    local seq = rng:seq()
     for d in util.pairs_by_keys(doors) do
         for _, entry in util.pairs_by_keys(self.obstacles[item][d] or {}) do
-            if bit32.band(mask, entry.mask) == 0 then
-                table.insert(entries, entry)
+            if bit32.band(mask, entry.mask) == 0 and seq(get_component_size(entry.mask) ^ 2) then
+                chosen = entry
             end
         end
     end
-    if #entries == 0 then
+    if chosen then
+        return chosen.id, bit32.bor(chosen.mask, zentropy.get_door_mask(dir))
+    else
         return
     end
-    local entry = entries[rng:random(#entries)]
-    return entry.id, bit32.bor(entry.mask, zentropy.get_door_mask(dir))
 end
 
 function zentropy.db.Components:get_filler(mask, rng)
-    local entries = {}
+    local chosen = nil
+    local seq = rng:seq()
     for _, entry in util.pairs_by_keys(self.fillers) do
-        if bit32.band(mask, entry.mask) == 0 then
-            table.insert(entries, entry)
+        if bit32.band(mask, entry.mask) == 0 and seq(get_component_size(entry.mask) ^ 2) then
+            chosen = entry
         end
     end
-    if #entries == 0 then
+    if chosen then
+        return chosen.id, chosen.mask
+    else
         return
     end
-    local entry = entries[rng:random(#entries)]
-    return entry.id, entry.mask
 end
 
 function zentropy.db.Components:get_puzzle(mask, rng)
-    local entries = {}
+    local chosen = nil
+    local seq = rng:seq()
     for _, entry in util.pairs_by_keys(self.obstacles.puzzle.northsoutheastwest) do
-        if bit32.band(mask, entry.mask) == 0 then
-            table.insert(entries, entry)
+        if bit32.band(mask, entry.mask) == 0 and seq(get_component_size(entry.mask) ^ 2) then
+            chosen = entry
         end
     end
-    if #entries == 0 then
+    if chosen then
+        return chosen.id, chosen.mask
+    else
         return
     end
-    local entry = entries[rng:random(#entries)]
-    return entry.id, entry.mask
 end
 
 function zentropy.db.Components:get_treasure(open, mask, rng)
@@ -361,23 +375,24 @@ function zentropy.db.Components:get_treasure(open, mask, rng)
     if not self.treasures[open] then
         return
     end
-    local entries = {}
+    local chosen = nil
+    local seq = rng:seq()
     for _, entry in util.pairs_by_keys(self.treasures[open]) do
         if entry.mask == 'any' then
             for _, section in ipairs(self.CORNER_SECTION_MASKS) do
-                if bit32.band(mask, section) == 0 then
-                    table.insert(entries, {mask=section, id=entry.id})
+                if bit32.band(mask, section) == 0 and seq(get_component_size(entry.mask) ^ 2) then
+                    chosen = {mask=section, id=entry.id}
                 end
             end
-        elseif bit32.band(mask, entry.mask) == 0 then
-            table.insert(entries, entry)
+        elseif bit32.band(mask, entry.mask) == 0 and seq(get_component_size(entry.mask) ^ 2) then
+            chosen = entry
         end
     end
-    if #entries == 0 then
+    if chosen then
+        return chosen.id, chosen.mask
+    else
         return
     end
-    local entry = entries[rng:random(#entries)]
-    return entry.id, entry.mask
 end
 
 function zentropy.db.Components:get_floors(rng)
