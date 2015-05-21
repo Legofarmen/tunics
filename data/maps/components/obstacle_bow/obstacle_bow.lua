99034d5e3d34a42fb50c510ef0f091216429b598
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-21 22:38:23 +0200

    Fix bow and lamp obstacle helpers

--- data/maps/components/obstacle_bow/obstacle_bow.lua
+++ data/maps/components/obstacle_bow/obstacle_bow.lua
@@ -4,69 +4,53 @@ local util = require 'lib/util'
 local zentropy = require 'lib/zentropy'
 
 function bow.init(map, data, timeout)
-	local doors = {}
-	for dir, door_data in util.pairs_by_keys(data.doors) do
+    local doors = {}
+    for dir, door_data in util.pairs_by_keys(data.doors) do
         assert((door_data.open or 'open') == 'open')
-		local door = data.room:door({open='closed', name=door_data.name, room_events=data.room_events}, dir)
+        local door = data.room:door({open='closed', name=door_data.name, room_events=data.room_events}, dir)
         table.insert(doors, door)
-	end
+    end
 
-    for entity in map:get_entities('enemy') do
+    for entity in map:get_entities('enemy_') do
         zentropy.inject_enemy(entity, data.rng:refine(entity:get_name()))
     end
 
-	for entity in map:get_entities('pot_') do
+    for entity in map:get_entities('pot_') do
         zentropy.inject_pot(entity, data.rng:refine(entity:get_name()))
     end
-	
+
     local hidden_chest = nil
 
-	if data.treasure1 then
-		local x, y = map:get_entity('treasure_obstacle_chest'):get_position()
-		x, y = x + 8, y + 13
-		hidden_chest = map:create_chest{
-            sprite="entities/chest",
-            layer=1,
-            x = x,
-            y = y,
-            treasure_name=data.treasure1.item_name,
-            treasure_savegame_variable=data.treasure1.name,
-        }
+    local treasure_obstacle_chest = map:get_entity('treasure_obstacle_chest')
+    if data.treasure1 then
+        hidden_chest = zentropy.inject_chest(treasure_obstacle_chest, data.treasure1)
         if not hidden_chest:is_open() then
             hidden_chest:set_enabled(false)
         end
-	else
-		map:set_entities_enabled('treasure_obstacle_', false)
-	end
+    else
+        map:remove_entities('treasure_obstacle_')
+    end
 
-	if data.treasure2 then
-		local x, y = map:get_entity('treasure_open_chest'):get_position()
-		x, y = x + 8, y + 13
-		map:create_chest{
-            sprite="entities/chest",
-            layer=1,
-            x = x,
-            y = y,
-            treasure_name=data.treasure2.item_name,
-            treasure_savegame_variable=data.treasure2.name,
-        }
-	else
-		map:set_entities_enabled('treasure_open_', false)
-	end
+    local treasure_open_chest = map:get_entity('treasure_open_chest')
+    if data.treasure2 then
+        zentropy.inject_chest(treasure_open_chest, data.treasure2)
+    else
+        map:remove_entities('treasure_open_')
+    end
 
-	local switch = map:get_entity('switch')
-	function switch:on_activated()
-		local sound = nil
-		if data.treasure1 then
-			hidden_chest:set_enabled(true)
-			sound = 'chest_appears'
-		end
-		for _, component in ipairs(doors) do
-			component:open()
-			sound = 'secret'
-		end
-		sol.audio.play_sound(sound)
-	end
+    local switch = map:get_entity('switch')
+    function switch:on_activated()
+        local sound = nil
+        if data.treasure1 then
+            hidden_chest:set_enabled(true)
+            sound = 'chest_appears'
+        end
+        for _, component in ipairs(doors) do
+            component:open()
+            sound = 'secret'
+        end
+        sol.audio.play_sound(sound)
+    end
 
     data.room_events:add_door_sensor_activated_listener(function ()
         if not switch:is_activated() then
