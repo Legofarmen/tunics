4ccc59ce7753fa84f15f50b01f025674c8e998ab
Jan Sandstrom <jan@legofarmen.se>
2015-05-23 02:31:06 +0200

    Better, but not great enemy/snap_dragon

--- data/enemies/snap_dragon.lua
+++ data/enemies/snap_dragon.lua
@@ -10,44 +10,54 @@ enemy:set_origin(8, 13)
 local sprite = enemy:create_sprite("enemies/snap_dragon")
 
 function enemy:on_restarted()
-
-  self:go_random()
+	self:go_random()
 end
 
 function enemy:on_movement_finished(movement)
+	sprite:set_animation("immobilized")
+	sol.timer.start(self, 600, function()
+		sprite:set_animation("bite")
+		sol.timer.start(200, function()
+			sprite:set_animation("immobilized")
+			sol.timer.start(600, function()
+				sprite:set_animation("bite")
+				sol.timer.start(200, function()
+					self:go_random()
+				end)
+			end)
+		end)
+	end)
 
-  self:go_random()
 end
 
 function enemy:on_obstacle_reached(movement)
+	self:get_movement():set_angle(self:set_direction())
+end
 
-  self:go_random()
+function enemy:set_direction()
+	local rand4 = math.random(4)
+	local direction8 = rand4 * 2 - 1
+	local angle = direction8 * math.pi / 4
+	
+	sprite:set_direction(rand4 - 1)
+	return angle
 end
 
 function enemy:go_random()
 
-  -- Random diagonal direction.
-  local rand4 = math.random(4)
-  local direction8 = rand4 * 2 - 1
-  local angle = direction8 * math.pi / 4
-  local m = sol.movement.create("straight")
-  m:set_speed(48)
-  m:set_angle(angle)
-  m:set_max_distance(24 + math.random(96))
-  m:start(self)
-
-  sprite:set_direction(rand4 - 1)
-
-  sol.timer.stop_all(self)
-  sol.timer.start(self, 300 + math.random(1500), function()
-    sprite:set_animation("bite")
-  end)
+	-- Random diagonal direction.
+	sprite:set_animation("walking")
+	local m = sol.movement.create("straight")
+	self:set_direction()
+	m:set_speed(math.random(32,72))
+	m:start(self)
+	m:set_angle(self:set_direction())
+	sol.timer.start(self, 2000, function()
+		m:stop()
+		self:on_movement_finished()
+	end)
 end
 
 function sprite:on_animation_finished(animation)
 
-  if animation == "bite" then
-    self:set_animation("walking")
-  end
 end
-
