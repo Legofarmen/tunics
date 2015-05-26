ac103ae44ed7ea965fd2f42ea8f02550256f42d5
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-26 09:50:32 +0200

    Work around separator-stream interaction issue

--- data/maps/components/door/door_closed.lua
+++ data/maps/components/door/door_closed.lua
@@ -2,18 +2,19 @@ local zentropy = require 'lib/zentropy'
 
 local door_closed = {}
 
-function door_closed.init(map, data, direction)
+function door_closed.init(map, data, direction4)
+    print(direction4)
 
     local door = zentropy.inject_door(map:get_entity('door_open'), {
         savegame_variable = data.name,
-        direction = direction,
+        direction = direction4,
         sprite = "entities/door_normal",
     })
 
     local sensor = map:get_entity('sensor')
 
     function sensor:on_activated()
-        data.room_events:on_door_sensor_activated(direction)
+        data.room_events:on_door_sensor_activated(direction4)
     end
 
     local component = {}
@@ -30,10 +31,23 @@ function door_closed.init(map, data, direction)
         map:set_doors_open(door:get_name(), open)
     end
 
+    local entering = map:get_entity('entering')
+    function entering:on_activated()
+        local hero = map:get_hero()
+        local m = sol.movement.create('straight')
+        m:set_speed(hero:get_walking_speed())
+        m:set_angle(math.pi / 2 * direction4 + math.pi)
+        m:set_max_distance(48)
+        function m:on_finished()
+            hero:unfreeze()
+        end
+        m:start(hero)
+    end
+
     component:set_open(true)
 
     function door:on_opened()
-        map:remove_entities('stream_')
+        map:remove_entities('entering')
     end
 
     return component
