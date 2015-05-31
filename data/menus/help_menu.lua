656503742b3f0f192a42d998f0bd42fd3ea6f102
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2015-05-31 02:14:40 +0200

    Hook up new help screens

--- data/menus/help_menu.lua
+++ data/menus/help_menu.lua
@@ -5,20 +5,27 @@ function help_menu:start(ctx)
 end
 
 function help_menu:on_started()
-    self.surface = sol.surface.create("menus/help.png")
+    self.help = {
+        sol.surface.create("menus/help_1.png"),
+        sol.surface.create("menus/help_2.png"),
+    }
+    self.current_item = 1
 end
 
 function help_menu:on_command_pressed(key)
     local handled = false
-    if key == "action" or key == "escape" then
+    if key == "escape" then
         sol.menu.stop(self)
+        handled = true
+    elseif key == "action" then
+        self.current_item = self.current_item % 2 + 1
         handled = true
     end
     return handled
 end
 
 function help_menu:on_draw(dst_surface)
-    self.surface:draw(dst_surface)
+    self.help[self.current_item]:draw(dst_surface)
 end
 
 return help_menu
