4bc77e2144a3943e1aa82a544616546fd549ba0e
sandstrand <jan@legofarmen.se>
2016-11-01 19:15:30 +0100

    Add hole candidates

--- data/lib/mappings.lua
+++ data/lib/mappings.lua
@@ -1,3 +1,189 @@
+local dungeon_styles = {
+	1 = {
+		-- swamp palace
+		-- palette = 'dungeon.brick.swamp_palace.1';
+		tier_introduction = 2,
+		tileset = 'dungeon.brick.swamp_palace',
+		room.floor = {
+			a = {
+				high = { 'floor.a.12.high', 'floor.a.6.high' },
+				low = { 'floor.a.13.low.' },
+				0.75,
+			},
+			b = {
+				high = { 'floor.b.12.high', },
+				low = { 'floor.a.13.low', },
+				0.25,
+			},
+		},
+		room.wall_pillars = {
+			a = {
+				socket = { 'pillar.8', },
+				socketless = { 'pillar.8.socketless', 8, 8 },
+				1,
+			}
+		},
+		room.drapes = null,
+		room.statues = {
+			a = { 'statue.3', 1 },
+		},
+		room.wall_statues = {
+			a = { 'wall_statue.5' , 0.8 },
+			b = { 'wall_hole.1', 0.2 },
+		},
+		room.barrier = {
+			a = { 'barrier.1', 1 },
+		},
+		room.big_barrier = {
+			a = { 'big_barrier.1', 1 },
+		},
+		room.stage = {
+			a = { 'stage.1', 1 },
+		},
+		room.entrance = {
+			a = { 'entrance.1', 1 },
+		},
+		room.entrance_statue = {
+			a = { 'entrance_statue.4', 1 },
+		},
+		dungeon.music = {
+			a = { 'dungeon_dark', 1 },
+		},
+		dungeon.destructibles = {
+			pot = 'entities/vase_skull',
+    	    stone1 = 'entities/stone_white_skull',
+        	stone2 = 'entities/stone_black_skull',
+        },
+	},
+	
+	2 = {
+		-- ganons tower
+		-- palette = 'dungeon.ganons_tower.ganons_tower.1';
+		tier_introduction = 6,
+		tileset = 'dungeon.ganons_tower_ganons_tower',
+		floor.room = {
+			1 = {
+				high = { 'floor.a.3.high' },
+				low = { 'floor.a.3.low.' },
+			},
+			2 = {
+				high = { 'floor.a.17.high', },
+				low = { 'floor.a.17.low', },
+			},
+			3 = {
+				high = { 'floor.b.19.high', },
+				low = { 'floor.b.19.low', },
+			},
+		},
+		floor.dungeon = {
+			a = { 1, 0.5 },
+			b = { 2, 0.35 },
+			c = { 3, 0.15 },
+		},
+		wall_pillars = {
+			a = { 'pillar.1', 1 },
+		},
+		wall_pillars.socketless = {
+			a = { 'pillar.1.socketless', 1, 8, 8 },
+		},
+		drapes = null,
+		statues = {
+			a = { 'statue.5', 1 },
+		},
+		wall_statues = {
+			a = { 'wall_statue.1' , 0.8 },
+			b = { 'wall_statue.2', 0.2 },
+		},
+		barrier = {
+			a = { 'barrier.2', 1 },
+		},
+		big_barrier = {
+			a = { 'big_barrier.2', 1 },
+		},
+		stage = {
+			a = { 'stage.2', 1 },
+		},
+		entrance = {
+			a = { 'entrance.1', 1 },
+		},
+		entrance_statue = {
+			a = { 'entrance_statue.8', 1 },
+		},
+		music = {
+			a = { 'dungeon_dark', 1 },
+		},
+		destructibles = {
+       		pot = 'entities/vase_skull',
+       		stone1 = 'entities/stone_white_skull',
+        	stone2 = 'entities/stone_black_skull',
+        },
+	},
+	3 = {
+		-- hyrule castle
+		-- palette = 'dungeon.hyrule_castle.hyrule_castle.1';
+		tier_introduction = 4,
+		tileset = 'dungeon.hyrule_castle.hyrule_castle',
+		floor.room = {
+			1 = {
+				high = { 'floor.a.11.high' },
+				low = { 'floor.a.11.low.' },
+			},
+			2 = {
+				high = { 'floor.b.11.high', },
+				low = { 'floor.b.11.low', },
+			},
+			3 = {
+				high = { 'floor.a.5.high', },
+				low = { 'floor.a.5.low', },
+			},
+		},
+		floor.dungeon = {
+			a = { 1, 0.40 },
+			b = { 2, 0.40 },
+			c = { 3, 0.20 },
+		},
+		wall_pillars = {
+			a = { 'pillar.8', 1 },
+		},
+		wall_pillars.socketless = {
+			a = { 'pillar.8', 1 },
+		},
+		drapes = {
+			a = { 'drapes.1', 1},
+		},
+		statues = {
+			a = { 'statue.2', 1 },
+		},
+		wall_statues = {
+			a = { 'wall_statue.7' , 0.75 },
+			b = { 'wall_statue.10', 0.25 },
+		},
+		barrier = {
+			a = { 'barrier.1', 1 },
+		},
+		big_barrier = {
+			a = { 'big_barrier.1', 1 },
+		},
+		stage = {
+			a = { 'stage.2', 1 },
+		},
+		entrance = {
+			a = { 'entrance.2', 1 },
+		},
+		entrance_statue = {
+			a = { 'entrance_statue.8', 1 },
+		},
+		music = {
+			a = { 'dungeon_castle', 1 },
+		},
+		destructibles = {
+     	   pot = 'entities/vase',
+     	   stone1 = 'entities/stone_white',
+     	   stone2 = 'entities/stone_black',
+        },
+	},
+}
+
 local family_tier = {
     cave = 1,
     brick = 2,
